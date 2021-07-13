e9cba6ee010efe82609eb3da112b240166081b2a
Hellshi <hcslimaa@gmail.com>
2021-07-13 14:00:48 -0300

    Added create file and file table

---
+++ app/Controllers/Http/FileController.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-unused-vars */
+'use strict'
+
+const File = use('App/Models/File')
+const helpers = use('Helpers')
+
+class FileController {
+  async store ({ request, response }) {
+    try {
+      console.log(request.file('file'))
+      if (!request.file('file')) return
+      console.log('chegou aqui')
+      const upload = request.file('file', { size: '2mb' })
+
+      const fileName = `${Date.now()}.${upload.subtype}`
+
+      await upload.move(helpers.tmpPath('uploads'), {
+        name: fileName
+      })
+
+      if (!upload.moved()) {
+        throw upload.error()
+      }
+
+      const file = await File.create({
+        file: fileName,
+        name: upload.clientName,
+        type: upload.type,
+        subtype: upload.subtype
+      })
+
+      return response.send(file)
+    } catch (err) {
+      console.log(err)
+      return response.status(err.status).send({ error: { mensage: err } })
+    }
+  }
+}
+
+module.exports = FileController
--- start/routes.js
+++ start/routes.js
@@ -6,3 +6,5 @@ Route.post('user', 'UserController.store')
 Route.post('session', 'SessionController.store')
 Route.post('forgot', 'ForgotPasswordController.store')
 Route.put('forgot', 'ForgotPasswordController.update')
+
+Route.post('file', 'FileController.store')
